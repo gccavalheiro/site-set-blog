ec5f1b12de1831c2d50f46d695e582fd733790d7
Gabriel Cavalheiro <gccavalheiro94@gmail.com>
2025-06-14 02:29:55 -0300

    added seo

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,34 @@
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export const revalidate = 60;
 
@@ -16,7 +38,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage(props: BlogPostPage) {
+export default async function BlogPostPage(props: BlogPostPageProps) {
   const { slug } = await props.params;
   const post = allPosts.find((post) => post.slug === slug);
 
